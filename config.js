b21693b1af44c933fcdd27db754314f2cffa8f09
Jacob Nobili <jnobili14@gmail.com>
2025-08-05 21:24:41 -0600

    added config to public folder

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+// This file contains shared configuration variables to be used across other scripts.
+// By defining the backendUrl here, we avoid declaring it multiple times.
+
+const backendUrl = 'https://crochet-backend-ho1l.onrender.com';
