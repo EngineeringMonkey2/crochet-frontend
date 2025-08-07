865bfc4ddab68b1c9d33129349dadedaf99df8a0
Jacob Nobili <jnobili14@gmail.com>
2025-08-07 01:00:02 -0600

    Updated products

--- products.js
+++ products.js
@@ -8,32 +8,31 @@ const root6 = "images/RealChrochet/succulent/"
 const root7 = "images/RealChrochet/lamb/"
 const root8 = "images/RealChrochet/frog/"
 const root9 = "images/RealChrochet/elephant/"
+const root10 = "images/RealChrochet/giraffe/"
 
 const products = [
   {
     id: 1,
     name: "Chocolate Cow",
     images: [root1 + "cow8.png", root1 + "cow13.png", root1 + "cow3.png", root1 + "cow12.png" , root1 + "cow1.png", root1+ "cow10.png"],
-    price: "$44.99",
-    description: "A cute hand made crocheted cow with floppy ears, perfect for your collection.",
-	dimensions: " Dimensions: 12 in. by 9 in. ",
-    reviews: [101, 102]
+    price: "$39.99",
+    description: "A cute hand made crocheted cow plushie with floppy ears, perfect for your collection.",
+	dimensions: "  Dimensions: Height of 12 in. Width of 5 in. "
   },
   {
     id: 2,
     name: "Bunny",
     images: [root2 + "bunny1.png", root2 + "bunny3.png", root2 + "bunny18.png", root2 + "bunny4.png" , root2 + "bunny2.png", root2 + "bunny5.png"],
-    price: "$44.99",
-    description: "A cute handmade crocheted bunny with floppy ears, perfect for your animal collection!",
-	dimensions: " Dimensions: 1ft. by 9 in. ",
-    reviews: [103]
+    price: "$39.99",
+    description: "A cute handmade crocheted bunny plushie with floppy ears, perfect for your animal collection!",
+	dimensions: "  Dimensions: Height of 11 in. Width of 4.5 in. "
   },
   {
     id: 3,
     name: "Oringinal Monkey",
     images: [root3 + "monkey1.png", root3 + "monkey3.png", root3 + "monkey2.png", root3 + "monkey4.png" , root3 + "monkey6.png", root3 + "monkey5.png"],
     price: "$44.99",
-    description: "The oringinal noncustomized monkey.",
+    description: "The oringinal handmade noncustomized monkey plushie.",
 	dimensions: " Dimensions: 12in. by 9 in. "
 
   },
@@ -42,8 +41,8 @@ const products = [
     name: "Strawberry Cow",
     images: [root4 + "strawberrycow1.png", root4 + "strawberrycow2.png"],
     price: "$44.99",
-    description: "A beautiful hand made strawberry cow, perfect for your collection.",
-	dimensions: " Dimensions: 12in. by 9 in. "
+    description: "A beautiful hand made strawberry cow plushie with spots, perfect for your collection.",
+	dimensions: "  Dimensions: Height of 12 in. Width of 5 in. "
 
   },
 
@@ -56,9 +55,9 @@ const products = [
     id: 5,
     name: "Lamb",
     images: [root7 + "lamb1.png", root7 + "lamb2.png", root7 + "lamb3.png", root7 + "lamb4.png", root7 + "lamb5.png", root7 + "lamb6.png"],
-    price: "$49.99",
-    description: "Beautiful lamb, perfect to add to your collection of animals",
-	dimensions: " Dimensions: coming soon "
+    price: "$44.99",
+    description: "Beautiful hand made lamb plushie, perfect to add to your collection of animals",
+	dimensions: " Dimensions: Height of 12 in. Width of 6 in. "
 
   },  
   
@@ -74,8 +73,8 @@ const products = [
     name: "Elephant",
     images: [root9 + "elephant1.png", root9 + "elephant2.png", root9 + "elephant3.png", root9 + "elephant4.png", root9 + "elephant5.png", root9 + "elephant6.png"],
     price: "$19.99",
-    description: "Tiny elephant with flappy ears, perfect for your collection",
-	dimensions: " Dimensions: coming soon "
+    description: "Small hand made elephant plushie with flappy ears, perfect for your collection",
+	dimensions: " Dimensions: Height of 7 in. Width of 4 in. Ear Diameter of 3.5 In."
 
   },  
   
@@ -91,8 +90,8 @@ const products = [
     name: "Frog",
     images: [root8 + "frog1.png", root8 + "frog2.png", root8 + "frog3.png", root8 + "frog4.png", root8 + "frog5.png"],
     price: "$19.99",
-    description: "Beautiful large succulent coasters, able to fit any cup size",
-	dimensions: " Dimensions: coming soon "
+    description: "Beautiful hand made large frog plushie, more colors coming soon to the collection!",
+	dimensions: " Dimensions: Height of 7 in. Width of 5 in. Circumference of 15 in."
 
   },  
   
@@ -108,8 +107,8 @@ const products = [
     name: "Bee",
     images: [root5 + "bee1.png", root5 + "bee5.png", root5 + "bee2.png", root5 + "bee6.png" , root5 + "bee3.png", root5 + "bee7.png"],
     price: "$14.99",
-    description: "Cook up some fun with this chef-themed crocheted monkey.",
-	dimensions: " Dimensions: 7 in. by 3 in. "
+    description: "Cute hand made bee plushie perfect for your collection!",
+	dimensions: " Dimensions: Length of 6 in. Width of 3 in. Circumference of 9.5 in. "
 
   },
   
@@ -125,9 +124,9 @@ const products = [
     id: 9,
     name: "Succulent Coasters",
     images: [root6 + "succoaster1.png", root6 + "succoaster2.png", root6 + "succoaster3.png", root6 + "succoaster4.png", root6 + "succoaster5.png", root6 + "succoaster6.png"],
-    price: "$19.99",
-    description: "Beautiful large succulent coasters, able to fit any cup size",
-	dimensions: " Dimensions: coming soon "
+    price: "$24.99",
+    description: "Four hand made beautiful large succulent coasters with a basket to store, able to fit any cup size!",
+	dimensions: " Dimensions: The coaster has a diameter of 6 in. The basket has a diameter of 4.5 and a wall hieght of  1 in."
 
   },
   
@@ -138,11 +137,11 @@ const products = [
   
   {
     id: 10,
-    name: "Flower Coasters",
-    images: ["images/RealChrochet/flowerbundle.png", "images/RealChrochet/coasterflower.png","images/RealChrochet/fourflowers.png" ],
-    price: "$24.99",
-    description: "A set of crocheted coasters to protect your surfaces in style.",
-	dimensions: " Dimensions: coming soon"
+    name: "Giraffe",
+    images: [root10 + "giraffe5.png", root10 + "giraffe2.png", root10 + "giraffe3.png", root10 + "giraffe4.png", root10 + "giraffe1.png", root10 + "giraffe6.png"],
+    price: "$59.99",
+    description: "Beautiful hand made giraffe plushie, biggest animal out of all the crochet animals!",
+	dimensions: " Dimensions: Height of 15 in. Width of 6 in."
 
   }
 ];
